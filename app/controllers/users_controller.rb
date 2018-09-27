3df2f22653222606f39aa83d64867c0fbd9e0d7d
poyichou <bob2006tw@gmail.com>
2018-09-27 21:34:52 +0800

    fix some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,25 @@ class UsersController < ApplicationController
     if session[:user9487].nil?
       redirect_to new_user_path
     else
-      @user = session[:user9487]
-      puts "@user=#{@user}"
+      load_user
       @user_posts = @user.posts
+      @post = Post.new
     end
   end
 
   def edit
     # edit password
-    @user = session[:user9487]
+    load_user
+  end
+  
+  def new
+    @user = User.new
   end
 
   def add_post
     @user.posts.build(params.require(:post).permit(:content))
     unless @user.save
-        store_messages(@user.errors.full_messages)
+      store_messages(@user.errors.full_messages)
     end
     redirect_to users_path
   end
@@ -36,42 +40,38 @@ class UsersController < ApplicationController
         redirect_to edit_user_path
       end
     else
-      store_messages(["Password and password confirmation no the same"])
+      store_messages(["Password and password confirmation not the same"])
       redirect_to edit_user_path
     end
   end
-  
-  def new
-    @user = User.new
-  end
 
   def login
-    loginer = login_params
+    loginer = params.require(:user).permit(:name, :password)
     @user = User.where("name = :name AND password = :password", {name: loginer[:name], password: loginer[:password]}).first
     if @user.nil?
       # note found
       store_messages(["Wrong user name or password"])
       redirect_to new_user_path
     else
-      session[:user9487] = @user
+      store_user(@user)
       redirect_to users_path
     end
   end
 
   def logout
-    session[:user9487] = nil
-      redirect_to users_path
+    store_user(nil)
+    redirect_to users_path
   end
 
   def create
-    newuser = new_user_params
+    newuser = params.require(:user).permit(:name, :email, :password, :password_confirm)
     if @user.nil?
       # no user name conflict, safe to create
       if newuser[:password].eql? newuser[:password_confirm]
         # success
         @user = User.new(:name => newuser[:name], :email => newuser[:email], :password => newuser[:password])
         if @user.save
-          session[:user9487] = @user
+          store_user(@user)
           redirect_to users_path
         else
           # save fail
@@ -96,43 +96,32 @@ class UsersController < ApplicationController
       @post.destroy
     end
     @user.destroy
-    session[:user9487] = nil
+    store_user(nil)
     redirect_to users_path
   end
 
   private
 
   def load_user
-    @user = session[:user9487]
-  end
-  def load_messages
-    if not session[:message87].nil?
-      @messages = []
-      session[:message87][:error].map { |hash|
-        @messages << hash[:message]
-      }
-      session[:message87] = nil
+    if session[:user9487].nil?
+      @user = nil
     else
-      @messages = nil
+      @user = User.where("name = ?", session[:user9487]["name"]).first
     end
   end
 
+  def store_user(user)
+    if user.nil?
+      session[:user9487] = nil
+    else
+      session[:user9487] = { :name => user.name, :email => user.email, :password => user.password }
+    end	
+  end
+  def load_messages
+    @messages = session[:message87]
+  end
+
   def store_messages(messages)
-        session[:message87] = serialize_error(messages)
-  end
-
-  def new_user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirm)
-  end
-
-  def login_params
-    params.require(:user).permit(:name, :password)
-  end
-
-  def serialize_error(messages)
-    all_messages = messages.each { |message|
-      {:message => message}
-    }
-    {:error => all_messages}
+    session[:message87] = messages
   end
 end
