f10b4caa12a4ce0973f8d0fa8df6b4600a54b552
poyichou <bob2006tw@gmail.com>
2018-09-27 22:47:58 +0800

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :users, except: [:show]
-  post "/users/login", to: "users#login"
-  post "/users/logout", to: "users#logout"
-  post "/users/add_post", to: "users#add_post"
+  resources :users, except: [:show] do
+    collection do
+      post :login
+      post :logout
+      post :add_post
+      #post :delete_post
+    end
+  end
+  #post "/users/login", to: "users#login"
+  #post "/users/logout", to: "users#logout"
+  #post "/users/add_post", to: "users#add_post"
+  #post "/users/delete_post", to: "users#delete_post"
 
   root "users#index"
 end
